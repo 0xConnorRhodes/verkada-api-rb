fc8dfd491e9911b2f58ffc1b98339b4c562e1ff2
Connor Rhodes <connor@rhodes.contact>
2024-12-22 19:29:11 -0600

    set get_api_token function to private

--- vapi.rb
+++ vapi.rb
@@ -11,24 +11,6 @@ class Vapi
     @token_expiry = nil
   end
 
-  def get_api_token
-    response = self.class.post('/token', {
-      headers: {
-        'Content-Type' => 'application/json',
-        'x-api-key' => @api_key
-      }
-    })
-
-    if response.success?
-      @token = response["token"]
-      # Token expires in 30 minutes, store expiry time 29:50 in the future
-      @token_expiry = Time.now + 1790
-      @token
-    else
-      raise "Failed to get token: #{response.code} - #{response.body}"
-    end
-  end
-
   def get_camera_data
     get_api_token if token_expired?
 
@@ -214,6 +196,24 @@ class Vapi
   end
 
   private
+
+  def get_api_token
+    response = self.class.post('/token', {
+      headers: {
+        'Content-Type' => 'application/json',
+        'x-api-key' => @api_key
+      }
+    })
+
+    if response.success?
+      @token = response["token"]
+      # Token expires in 30 minutes, store expiry time 29:50 in the future
+      @token_expiry = Time.now + 1790
+      @token
+    else
+      raise "Failed to get token: #{response.code} - #{response.body}"
+    end
+  end
 
   def token_expired?
     return true if @token.nil? || @token_expiry.nil?
