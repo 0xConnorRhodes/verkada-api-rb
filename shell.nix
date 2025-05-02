9037f04e8515007a26c61de278ed3e58cb94b963
Connor Rhodes <connor@rhodes.contact>
2025-05-01 21:59:11 -0500

    move nix files .nix

--- shell.nix
+++ shell.nix
@@ -5,13 +5,12 @@ let
   httparty = bundlerEnv {
     name = "httparty";
     ruby = ruby_3_4;
-    gemdir = ./httparty;
+    gemdir = ./.nix;
   };
 in mkShell {
   name = "bundler-shell";
   buildInputs = [httparty bundix ruby_3_4];
   packages = with pkgs; [ 
-    cowsay 
     rubyPackages_3_4.pry
   ];
 }
