d0fee002b92acd9b3af41eca02cc6a7e7682ec68
Connor Rhodes <connor@rhodes.contact>
2025-05-14 10:15:23 -0500

    add get_camera_data test

--- tests/get_camera_data.rb
+++ tests/get_camera_data.rb
@@ -5,10 +5,12 @@ require 'pry'
 secrets_file = File.read(File.join(ENV['code_dir'], 'tests', 'secrets.json'))
 secrets = JSON.parse(secrets_file, symbolize_names: true)
 
-api_key = secrets[:apiKeys][:sewr]
+api_key = secrets[:apiKeys][:atx_demo_room]
+
 api = Vapi.new(api_key)
 
-# get last hour of audit log events, force multiple pages
-log = api.get_audit_logs(start_time: (Time.now - 60*60).to_i)
+cams = api.get_camera_data
 
-pp log
+# pp cams
+
+binding.pry
